5a7313acb5495b27e4cddecca8f26cf4008b64f3
luchen <813502730@qq.com>
2020-09-22 21:37:24 +0800

    init: 初始化基本路由login home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/login/login';
+import Home from './pages/home/home';
+
+import 'minireset.css';
 import './App.less';
 
-import SysLayout from './component/SysLayout/SysLayout'
-
 function App() {
-  return (  
+  return (
     <div className="App">
-      <SysLayout/>
+      <BrowserRouter>
+        <Switch>
+          {/* exact 表示精确匹配 */}
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
